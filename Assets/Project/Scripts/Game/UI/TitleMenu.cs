066ff416eece780b843949a5d1a3493fed115812
RollingK0824 <garamei72@gmail.com>
2026-07-11 12:10:52 +0900

    feat: UI and Title 복구

---
+++ Assets/Project/Scripts/Game/UI/TitleMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    [Header("게임 씬 이름 (Build Settings에 등록된 이름과 정확히 일치해야 함)")]
+    [SerializeField] private string gameSceneName;
+
+    [Header("옵션 패널")]
+    [SerializeField] private GameObject optionsPanel;
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnClickOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void OnClickCloseOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
