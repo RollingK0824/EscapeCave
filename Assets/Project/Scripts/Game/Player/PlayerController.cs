b46edf185351bd3c8f70d8450527671dd5f6a247
pajupaju0907-byte <pajupaju0907@gmail.com>
2026-07-16 09:21:21 +0900

    fix: 플레이어 컨트롤러 충돌버그
    버그수정

--- Assets/Project/Scripts/Game/Player/PlayerController.cs
+++ Assets/Project/Scripts/Game/Player/PlayerController.cs
@@ -38,14 +38,14 @@ public class PlayerController : MonoBehaviour, IDamageable
         _animator = GetComponent<Animator>();
         // 몬스터 관련 로직 추가
         Managers.MonsterManager.RegisterPlayer(transform);
-        if (jump != null)
+        if (_jump != null)
         {
-            jump.OnLanded += HandleLanded;
+            _jump.OnLanded += HandleLanded;
         }
     }
 
     // 몬스터 관련 로직 추가
-    private void HandleLanded()
+    private void HandleLanded(float fallDistance)
     {
         Managers.MonsterManager.NotifyVibration();
     }
@@ -122,9 +122,9 @@ public class PlayerController : MonoBehaviour, IDamageable
     private void OnDestroy()
     {
         Managers.MonsterManager.UnregisterPlayer(transform);
-        if (jump != null)
+        if (_jump != null)
         {
-            jump.OnLanded -= HandleLanded;
+            _jump.OnLanded -= HandleLanded;
         }
     }
 }
