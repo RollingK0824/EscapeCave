2ca898f1504a57ca442246f21a074b7fe2fa0da1
RollingK0824 <garamei72@gmail.com>
2026-07-22 18:33:47 +0900

    chore: TitleScene ScoreBoard Btn Add & ScoreBoardScene 유저 식별 요소 추가
    - ScoreBoardScene에 유저 식별용 Text 추가
    - TitleScene ScoreBoard Scene 이동 Button 추가

--- Assets/Project/Scripts/Game/UI/TitleMenu.cs
+++ Assets/Project/Scripts/Game/UI/TitleMenu.cs
@@ -5,13 +5,16 @@ using Managers;
 public class TitleMenu : MonoBehaviour
 {
     [Header("게임 씬 이름 (Build Settings에 등록된 이름과 정확히 일치해야 함)")]
-    [SerializeField] private string gameSceneName;
+    [SerializeField] private string _gameSceneName;
 
     [Header("해금 트리 씬 이름 (Build Settings에 등록된 이름과 정확히 일치해야 함)")]
-    [SerializeField] private string unlockTreeSceneName;
+    [SerializeField] private string _unlockTreeSceneName;
+
+    [Header("Score Board 씬 이름 (Build Settings에 등록된 이름과 정확히 일치해야 함)")]
+    [SerializeField] private string _scoreBoardScene;
 
     [Header("옵션 패널")]
-    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject _optionsPanel;
 
     public void OnClickStart()
     {
@@ -19,22 +22,27 @@ public class TitleMenu : MonoBehaviour
         {
             UIManager.Instance.StartNewSession();
         }
-        SceneManager.LoadScene(gameSceneName);
+        SceneManager.LoadScene(_gameSceneName);
     }
 
     public void OnClickUnlockTree()
     {
-        SceneManager.LoadScene(unlockTreeSceneName);
+        SceneManager.LoadScene(_unlockTreeSceneName);
+    }
+
+    public void OnClickScoreBoard()
+    {
+        SceneManager.LoadScene(_scoreBoardScene);
     }
 
     public void OnClickOptions()
     {
-        optionsPanel.SetActive(true);
+        _optionsPanel.SetActive(true);
     }
 
     public void OnClickCloseOptions()
     {
-        optionsPanel.SetActive(false);
+        _optionsPanel.SetActive(false);
     }
 
     public void OnClickQuit()
